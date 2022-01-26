9177c60de217d2b9afec9faea4b988fbb84bfea9
Ícaro Chagas <icaro.chagas.a@gmail.com>
2022-01-26 02:03:27 -0300

    Update seller and custom service exception

--- ServiceWebMvc/Controllers/SellersController.cs
+++ ServiceWebMvc/Controllers/SellersController.cs
@@ -2,10 +2,8 @@
 using ServiceWebMvc.Models;
 using ServiceWebMvc.Models.ViewModels;
 using ServiceWebMvc.Services;
-using System;
+using ServiceWebMvc.Services.Exceptions;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ServiceWebMvc.Controllers
 {
@@ -79,6 +77,47 @@ namespace ServiceWebMvc.Controllers
             }
 
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
 
 
---
+++ ServiceWebMvc/Services/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ServiceWebMvc.Services.Exceptions
+{
+    public class NotFoundException : AccessViolationException
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
