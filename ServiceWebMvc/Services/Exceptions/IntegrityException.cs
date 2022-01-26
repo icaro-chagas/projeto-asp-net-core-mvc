68a19d32c1d775c5d15b4b806fe4a5f24e87681a
Ícaro Chagas <icaro.chagas.a@gmail.com>
2022-01-26 03:46:17 -0300

    Exception handling for delete (referential integrity)

---
+++ ServiceWebMvc/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ServiceWebMvc.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
