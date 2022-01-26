e910660178727526d66bc5009aca3da06ec548d6
Ícaro Chagas <icaro.chagas.a@gmail.com>
2022-01-26 00:46:20 -0300

    Foreign key not null (referential integrity)

--- ServiceWebMvc/Migrations/20220126034009_DepartmentForeignKey.Designer.cs
+++ ServiceWebMvc/Migrations/20220126034009_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ServiceWebMvc.Models;
 
 namespace ServiceWebMvc.Migrations
 {
     [DbContext(typeof(ServiceWebMvcContext))]
-    partial class ServiceWebMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20220126034009_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
